b6e56d5b2fb357d8bf450e0ba3dbe99e7912afbb
Dan Armendariz <danallan@cs.harvard.edu>
2016-08-01 16:18:32 +0000

    first commit: gdb50args c9 command that spawns new proc window

---
+++ README.md
@@ -0,0 +1,1 @@
+# harvard.cs50.debug
---
+++ debug.js
@@ -0,0 +1,141 @@
+define(function(require, exports, module) {
+    main.consumes = ["Plugin", "commands", "dialog.error", "run", "run.gui"];
+    main.provides = ["harvard.cs50.debug"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+        var commands = imports.commands;
+        var run = imports.run;
+        var rungui = imports["run.gui"];
+        var showError = imports["dialog.error"].show;
+
+        /***** Initialization *****/
+        var plugin = new Plugin("Ajax.org", main.consumes);
+
+        function gdb50ExecCommand() {
+            run.addRunner("gdb50", {
+                caption: "gdb50",
+                debugger: "gdb",
+                $debugDefaultState: true,
+                executable: '"$file"',
+                retryCount: 100,
+                retryInterval: 300,
+                script: ['node /home/ubuntu/bin/c9gdbshim.js "$file" $args'],
+                socketpath: "/home/ubuntu/.c9/gdbdebugger.socket"
+            }, run);
+
+            commands.addCommand({
+                name: "gdb50args",
+                hint: "Pass args via command",
+                group: "General",
+                exec: function(args) {
+                    console.log("gdb50args",args);
+                    if (args.length < 2)
+                        showError("Please enter a filename to debug!");
+
+                    var src = args[1];
+
+                    // concat any args
+                    var exec = src;
+                    if (args.length > 2)
+                        exec += " " + args.slice(2).join(" ");
+
+                    // set runner and command as "last run", and execute it
+                    run.getRunner("gdb50", function(err, runner) {
+                        if (err)
+                            return console.log(err);
+
+                        rungui.lastRun = [runner, exec];
+                        commands.exec("runlast");
+                    });
+
+                    // run.detectRunner({ path: src }, function(err, runner) {
+                    //     if (err)
+                    //         showError("Could not find a runner for " + src + "!");
+
+                    //     console.log(runner);
+
+                    //     // force the debugger to show
+                    //     runner.$debugDefaultState = true;
+
+                    //     // concat any args
+                    //     var exec = src;
+                    //     if (args.length > 2)
+                    //         exec += " " + args.slice(2).join(" ");
+
+                    //     // set runner and command as "last run", and execute it
+                    //     rungui.lastRun = [runner, exec];
+                    //     commands.exec("runlast");
+                    // });
+                }
+            }, plugin);
+
+            var i = 0;
+            commands.addCommand({
+                name: "gdb50start",
+                hint: "running our debugger",
+                group: "General",
+                exec: function () {
+                    console.log(tabManager.focussedTab.document.getSession());
+                    process = debug.run({
+                        "debugger": "gdb"
+                    }, {
+                        path: "",
+                        cwd: "",
+                        env: {},
+                        args: [],
+                        debug: true
+                    }, "__cs50outputgdbdummy"+ (i++), function(err) {
+                        process.running = process.STARTED;
+                        console.log(err);});
+                }
+            }, plugin);
+
+            commands.addCommand({
+                name: "gdb50stop",
+                hint: "stopping our debugger",
+                group: "General",
+                exec: function () {
+                    console.log(process);
+                    debug && debug.stop();
+                    process.cleanUp();
+                    process && process.stop(function(err) {
+                        // process.emit("detach");
+                        // process.cleanup();
+                        err && console.log(err);
+                        console.log(process);
+                    });
+
+                }
+            }, plugin);
+        }
+
+        function load() {
+            gdb50ExecCommand();
+        }
+
+        /***** Methods *****/
+
+
+
+        /***** Lifecycle *****/
+
+        plugin.on("load", function() {
+            load();
+        });
+        plugin.on("unload", function() {
+
+        });
+
+        /***** Register and define API *****/
+
+        plugin.freezePublicAPI({
+
+        });
+
+        register(null, {
+            "harvard.cs50.debug": plugin
+        });
+    }
+});
