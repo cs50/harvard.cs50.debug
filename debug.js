7de488dd191648ad810090cca363ae176918e609
Dan Armendariz <danallan@cs.harvard.edu>
2016-08-03 01:08:10 +0000

    add process management support to properly handle multiple runs of gdb50start

--- debug.js
+++ debug.js
@@ -22,7 +22,9 @@ define(function(require, exports, module) {
 
         /***** Initialization *****/
         var plugin = new Plugin("Ajax.org", main.consumes);
-        var process = null;
+        var process = [];
+        var debugging = true;
+        var subsequent = null;
 
         /***** Methods *****/
 
@@ -45,7 +47,7 @@ define(function(require, exports, module) {
             // Monitors a shim started on the command line.
             run.addRunner("Shell50", {
                 caption: "Shell50",
-                script: ['while kill -0 $(pgrep -fn c9gdbshim.js); do sleep 1; done'],
+                script: ['while kill -0 $args ; do sleep 1; done'],
                 debugger: "gdb",
                 $debugDefaultState: true,
                 retryCount: 100,
@@ -89,6 +91,30 @@ define(function(require, exports, module) {
             showError(proc, "error:", err);
         }
 
+        function startProxy(cwd, pid, runner) {
+            // start proxy process
+            var procOpts = {
+                cwd: cwd,
+                args: [pid.toString()],
+                debug: true,
+            };
+            process[pid] = run.run(runner, procOpts, function(err) {
+                if (err)
+                    return handleErr("Proxy process run", err);
+
+                // once running, debug
+                debug.debug(process[pid], function(err) {
+                    if (err) {
+                        handleErr("Debug start", err);
+                        process[pid].stop();
+                        delete process[pid];
+                    }
+
+                    debugging = true;
+                });
+            });
+        }
+
         /**
          * Start a process that serves as a proxy for a GDB shim
          * already running on the command line. The proxy simply
@@ -99,6 +125,14 @@ define(function(require, exports, module) {
          *  c9 exec gdb50stop`
          */
         function gdb50Start(args) {
+            if (args.length != 2) {
+                showError("Error: expected process PID!");
+                return false;
+            }
+
+            // process pid passed by argument
+            var pid = args[1];
+
             // fetch shell runner
             run.getRunner("Shell50", function(err, runner) {
                 if (err)
@@ -106,22 +140,13 @@ define(function(require, exports, module) {
 
                 // make sure debugger isn't already running
                 debug.checkAttached(function() {
-                    // start proxy process
-                    var procOpts = {
-                        cwd: args[0],
-                        args: [],
-                        debug: true,
-                    };
-                    process = run.run(runner, procOpts, function(err) {
-                        if (err)
-                            return handleErr("Proxy process run", err);
+                    console.log("ATTACHING", process.length);
+                    // no cli process running
+                    if (!debugging)
+                        return startProxy(args[0], pid, runner);
 
-                        // once running, debug
-                        debug.debug(process, function(err) {
-                            if (err)
-                                return handleErr("Debug start", err);
-                        });
-                    });
+                    // wait to startProxy until old has stopped
+                    subsequent = startProxy.bind(this, args[0], pid, runner);
                 });
             });
         }
@@ -130,14 +155,27 @@ define(function(require, exports, module) {
          * gdb50Stop
          * Stops and cleans a debug process started with gdb50Start.
          */
-        function gdb50Stop() {
+        function gdb50Stop(args) {
+            if (args.length != 2) {
+                showError("Error: expected process PID!");
+                return false;
+            }
+
+            // process pid passed by argument
+            var pid = args[1];
+
             // must only run if a process is running
-            if (process === null)
+            if (process[pid] === undefined)
                 return false;
 
             debug.stop();
-            process.stop(function() {
-                process = null;
+            process[pid].stop(function() {
+                delete process[pid];
+                debugging = false;
+                if (subsequent) {
+                    subsequent();
+                    subsequent = null;
+                }
             });
         }
 
@@ -178,6 +216,8 @@ define(function(require, exports, module) {
         });
         plugin.on("unload", function() {
             process = null;
+            subsequent = null;
+            debugging = false;
         });
 
         /***** Register and define API *****/
