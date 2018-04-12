3b33da97b23a397660ee60e2d416dbd74bdf36a4
Kareem Zidane <kzidane@cs50.harvard.edu>
2018-04-11 21:54:28 -0400

    resetting IFS

--- debug.js
+++ debug.js
@@ -47,7 +47,7 @@ define(function(require, exports, module) {
         var SETTING_VER="project/cs50/debug/@ver";
 
         // version of debug50 file
-        var DEBUG_VER=13;
+        var DEBUG_VER=14;
 
         /***** Methods *****/
 
